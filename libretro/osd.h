296ea0dc291b5a7d0cb0a0e9229d5f5fee4154eb
StormedBubbles <80055191+StormedBubbles@users.noreply.github.com>
2023-08-26 20:20:08 -0400

    Update osd.h

--- libretro/osd.h
+++ libretro/osd.h
@@ -130,6 +130,8 @@ typedef struct
   uint8 render;
   t_input_config input[MAX_INPUTS];
   uint8 invert_mouse;
+  float xe1apxratio;
+  float xe1apyratio;
   uint8 invert_xe1ap;
   uint8 gun_cursor;
   uint32 overclock;
